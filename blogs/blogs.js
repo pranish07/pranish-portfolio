73e0ab07dec356b34f0b0619b1755cbd53fb2fc5
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-03-21 22:23:35 +0545

    feat: blogs designed

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -1,32 +1,44 @@
 const BlogContainer = document.querySelector("ol.blog-list-container");
 const url = "https://dev.to/api/articles?username=pranish07";
 fetch(url)
-.then(response => response.json())
-.then(blogs=>{
+  .then((response) => response.json())
+  .then((blogs) => {
     updateBlogList(blogs);
-    console.log(blogs);
-})
-function updateBlogList(blogs){
-    BlogContainer.innerHTML = blogs.map(post=>{
-        return`<li class="blog-post"> 
-        <div class="blog-post-container"></div>
-        <h1 class="blog-title">${post.title}</h1>
-        <p class="blog-tags">${
-            post.tag_list.map(tags => {
-                return `<span>${tags}</span>`
-            }).join("") 
-        }</p>
+  });
+function updateBlogList(blogs) {
+  BlogContainer.innerHTML = blogs.map((post) => {
+    return `<li class="blog-post"> 
+        <div class="blog-post-container ">
+        <h3 class="blog-title">${post.title}</h3>
+        <p class="">${post.tag_list
+          .map((tags) => {
+            return `<span class="link-primary blog-tags">${tags}</span>`;
+          })
+          .join("")}</p>
         <img class="blog-image" src=${post.social_image}>
         <p class="blog-desc">${post.description}</p>
-<div class="blog-public">
-        <p class="blog-date">${post.readable_publish_date}</p>
-        <p class="blog-time">${post.reading_time_minutes} Minutes</p>
-        <p class="blog-reaction">${post.positive_reactions_count} </p>
-        <p class="blog-comments">${post.comments_count}</p>
-        <a class="blog-url" href=${post.url}>Read More</a>
-     </div>   
-        
-        
-        </li>`
-    })
+        <div class="blog-public">
+            <div class="align-left">
+            <p class="blog-date "> <i class="fas fa-calendar mr-2"></i>${
+              post.readable_publish_date
+            }</p>
+                <p class="blog-time"> <i class="fas fa-clock mr-2"></i>${
+                  post.reading_time_minutes
+                } Minutes</p>
+                <p class="blog-reaction"><i class="fas fa-thumbs-up mr-2"></i>${
+                  post.positive_reactions_count
+                } </p>
+                <p class="blog-comments"><i class="fas fa-comment mr-2"></i>${
+                  post.comments_count
+                }</p>
+            </div>
+            <div >
+                <a class="blog-url text-decoration-none link-primary" href=${
+                  post.url
+                } target="_blank">Read More</a>
+            </div>   
+        </div>
+     </div>       
+        </li>`;
+  });
 }
